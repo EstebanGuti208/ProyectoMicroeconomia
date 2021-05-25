ed55969ea67b90ffed099af358f22193e37e06b8
Admin <admin@MacBook-Air-de-Esteban.local>
2021-05-24 21:07:17 -0500

    Varios cambios a la pagina, ya funciona la simulacion y tiene navegacion

--- js/index.js
+++ js/index.js
@@ -1,22 +1,69 @@
-botonPaginaProblema = document.querySelector('#botonPaginaProblema')
-botonPaginaSolucion = document.querySelector('#botonPaginaSolucion')
-botonPaginaImplementacion = document.querySelector('#botonPaginaImplementacion')
+console.log("It is working");
 
-botonPaginaProblema.onclick = function() {
-    document.getElementById('paginaProblema').style.display="visible";
-    document.getElementById('paginaSolucion').style.visibility="block";
-    document.getElementById('paginaImplementacion').style.visibility="block";
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Use buttons to toggle between views
+    document.querySelector('#botonPaginaProblema').addEventListener('click', () => cargarProblema());
+    document.querySelector('#botonPaginaSolucion').addEventListener('click', () => cargarSolucion());
+    document.querySelector('#botonPaginaImplementacion').addEventListener('click', () => cargarImplementacion());
+    document.querySelector('#botonASolucion').addEventListener('click', () => cargarSolucion());
+    document.querySelector('#botonAImplementacion').addEventListener('click', () => cargarImplementacion());
+    document.querySelector('#desarrolloSimulacion').addEventListener('click', () => simulacion());
+
+  
+    // By default, load the inbox
+    cargarProblema();
+  });
+
+
+function simulacion () {
+    const precioChoque = 2333700
+    const numeroChoques = parseInt(document.querySelector('#selectorSimulacion').value);
+    console.log(numeroChoques)
+    var costoAseguradoraNueva = numeroChoques * precioChoque * 0.5;
+    var choques = [];
+    var total = 0;
+    for(let i = 0; i < numeroChoques; i++){
+        
+        choques.push(Math.floor(Math.random() * (4000000 - 667400 + 1)) + 667400)
+    }
+    for(let y = 0; y < numeroChoques; y++){
+        total += choques[y];
+    }
+    var costoAseguradoraVieja = total / 2;
+    console.log(total)
+    console.log(costoAseguradoraNueva)
+    console.log(costoAseguradoraVieja)
+    document.querySelector('#montoActual').innerHTML = costoAseguradoraVieja;
+    document.querySelector('#montoNuevo').innerHTML = costoAseguradoraNueva;
 }
 
-botonPaginaSolucion.onclick = function() {
-    document.getElementById('paginaProblema').style.display="block";
-    document.getElementById('paginaSolucion').style.visibility="visible";
-    document.getElementById('paginaImplementacion').style.visibility="block";
+function toTheTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
 }
 
-botonPaginaImplementacion.onclick = function() {
-    document.getElementById('paginaProblema').style.display="block";
-    document.getElementById('paginaSolucion').style.visibility="block";
-    document.getElementById('paginaImplementacion').style.visibility="visible";
+function cargarProblema() {
+    document.querySelector('#paginaProblema').style.display = 'block';
+    document.querySelector('#paginaSolucion').style.display = 'none';
+    document.querySelector('#paginaImplementacion').style.display = 'none';
+
+    toTheTop()
+}
+
+function cargarSolucion() {
+    document.querySelector('#paginaProblema').style.display = 'none';
+    document.querySelector('#paginaSolucion').style.display = 'block';
+    document.querySelector('#paginaImplementacion').style.display = 'none';
+
+    toTheTop()
+}
+
+function cargarImplementacion() {
+    document.querySelector('#paginaProblema').style.display = 'none';
+    document.querySelector('#paginaSolucion').style.display = 'none';
+    document.querySelector('#paginaImplementacion').style.display = 'block';
+
+    toTheTop()
 }
